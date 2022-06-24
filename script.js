b814e13069dda3150c5458c263a9620cb6cc567d
Teagrin Forde <teagrinb@gmail.com>
2022-06-24 19:02:36 -0400

    Update functions in js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // generate button
 var generateBtn = document.querySelector("#generate");
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; //number array
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; //uppercase array
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //lowercase array
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+', '/', '?', '.', '>', ',', '<', '`', '~', '|']; //special characters array
 
 // Write password to the #password input
 function writePassword() {
@@ -16,36 +20,46 @@ generateBtn.addEventListener("click", writePassword);
 //Generate password to match the given criteria
 function generatePassword() {
   console.log("Hey! you clicked the button.");
-  let aNumber = Number(window.prompt("How many characters would you like your password to contain?"),"");   //prompt user for password length
 
-  if(aNumber >= 8 && aNumber <= 128) {  //password length 8-128
-    let pswdLength = aNumber;
+  var aNumber = Number(window.prompt("How many characters would you like your password to contain?"), "");   //prompt user for password length
+
+  if (aNumber >= 8 && aNumber <= 128) {  //password length 8-128
+    var pswdLength = aNumber;
+    console.log(pswdLength);
+    var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+', '/', '?', '.', '>', ',', '<', '`', '~', '|'];
+    characters.length = pswdLength; // set pswdLength to characters
+
+    if (window.confirm("Click OK to confirm including numeric characters.")) { //prompt numbers
+      console.log('num = true');
+      characters.concat(numeric);  //add numeric string to characters
+    }
+    if (window.confirm("Click OK to confirm including lowercase characters.")) { //prompt lowercase
+      console.log('lower = true');
+      characters.concat(lowercase); //add lowercase string to characters
+    }
+    if (window.confirm("Click OK to confirm including uppercase characters.")) { //prompt uppercase
+      console.log('upper = true');
+      characters.concat(uppercase); //add uppercase string to characters
+    }
+    if (window.confirm("Click OK to confirm including special characters.")) { //prompt special characters
+      console.log('spec = true');
+      characters.concat(special); //add special string to characters
+    } else {
+      alert("You must choose at least one type of character."); //validate input at least one character type selected
+      generatePassword(); //reset prompt
+    }
   } else {
     alert("INCORRECT LENGTH - You must choose from 8 to 128 characters."); //length validation
-    //reset prompt
-  };
-
-  if(window.confirm("Click OK to confirm including numeric characters.")) { //prompt numbers
-    function getRndInteger() {
-      return Math.floor(Math.random()*10);
-    };
-  }
-  if(window.confirm("Click OK to confirm including lowercase characters.")) { //prompt lowercase
-    let lowercase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-  }
-  if(window.confirm("Click OK to confirm including uppercase characters.")) { //prompt uppercase
-    let uppercase = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
-  }
-  if(window.confirm("Click OK to confirm including special characters.")) { //prompt special characters
-    let special = charset
+    generatePassword(); //reset prompt
   }
 
-  //validate the input - at least one character type should be selected
+    //generated password genPswd should match the criteria 
+    function shuffleArray(characters) {
+      characters.sort(() => Math.random()-0.5);
+    }
+    shuffleArray(characters); // shuffle the characters
 
+    var genPswd = characters;
 
-  //generated password genPswd should match the criteria 
-  let genPswd = function () {
-  }
-      
-  return genPswd;   // generated password written to the page
+  return genPswd   // generated password written to the page
 }
